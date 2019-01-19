38d5b1b7a1ee91483d53e1e9c34112eb874e7ace
markobalogh <12981558+markobalogh@users.noreply.github.com>
2019-01-18 18:13:31 -0800

    ~

--- src/temp.ts
+++ src/temp.ts
@@ -1,8 +1,8 @@
-import { ABT } from './abt';
-import { NearestNeighbors } from './nearestNeighbors';
-import { EuclideanDistanceMetric } from './distanceMetric';
-// import { LeaveOneOutCrossValidator } from './crossValidator';
-let dataset = new ABT().fromFile('data/derived/stfReturn/daily/finalABT.csv');
-let algo = new NearestNeighbors().withDistanceMetric('euclidean').withDistanceWeighting('generalizedGaussian').withZeroDistanceHandling('continue').withExponent(2).withSigma(0.01);
+// import { ABT } from './abt';
+// import { NearestNeighbors } from './nearestNeighbors';
+// import { EuclideanDistanceMetric } from './distanceMetric';
+// // import { LeaveOneOutCrossValidator } from './crossValidator';
+// let dataset = new ABT().fromFile('data/derived/stfReturn/daily/finalABT.csv');
+// let algo = new NearestNeighbors().withDistanceMetric('euclidean').withDistanceWeighting('generalizedGaussian').withZeroDistanceHandling('continue').withExponent(2).withSigma(0.01);
 
-// let model = new LeaveOneOutCrossValidator().query([algo, dataset])
+// // let model = new LeaveOneOutCrossValidator().query([algo, dataset])
